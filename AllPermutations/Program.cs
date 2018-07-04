3a42fed405b9ede6a5dfb05d4904435f5f172632
Ben Villalobos <villalobosb93@gmail.com>
2018-07-04 10:55:53 -0700

    Backtracking complete.
    Prints all permutations of a given string

---
+++ AllPermutations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllPermutations("ABC", "");
+            Console.ReadKey();
+        }
+
+        //Backtracking:
+        //make a choice
+        //explore the options
+        //undo the choice
+        static void AllPermutations(string data, string chosen)
+        {
+            if (data.Length == 0)
+            {
+                Console.WriteLine(chosen);
+            }
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                chosen += data[i];
+                data = data.Remove(i, 1);
+
+                AllPermutations(data, chosen);
+
+                data = data.Insert(i, chosen[chosen.Length - 1].ToString());
+                chosen = chosen.Remove(chosen.Length - 1);
+            }
+        }
+    }
+}
